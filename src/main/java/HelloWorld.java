47a638aac40ac68e4170e99eb78cde2224891bae
Rostyslav Krupelnytskyi <r0st1kkr86@gmail.com>
2023-07-31 23:22:46 +0300

    Changed HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello GoIT World!");
+        System.out.println("This is our second commit.");
 
     }
 
